671e8499067c9222a8190a482339b295d6f282b3
rjdgamer <40226268+rjdgamer@users.noreply.github.com>
2023-04-17 11:45:03 -0700

    letter.ts is correct

--- wordle-web/src/scripts/letter.ts
+++ wordle-web/src/scripts/letter.ts
@@ -1,36 +1,7 @@
 // Class that represents a letter in the wordle word and status
 // of the letter in the wordle word
 export enum LetterStatus {
-<<<<<<< HEAD
-    NotGuessed = 0,
-    Correct,
-    Misplaced,
-    Wrong
-}
 
-
-export class Letter {
-    letter: string;
-    status: LetterStatus = LetterStatus.NotGuessed;
-
-    constructor(letter: string) {
-        this.letter = letter;
-    }
-
-    get color() {
-        switch (this.status) {
-            case LetterStatus.Correct:
-                return 'green';
-            case LetterStatus.Misplaced:
-                return 'orange';
-            case LetterStatus.Wrong:
-                return 'red';
-            default:
-                return 'grey';
-        }
-    }
-}
-=======
   NotGuessed = 0,
   Correct,
   Misplaced,
@@ -58,4 +29,3 @@ export class Letter {
     }
   }
 }
->>>>>>> c3a9412d1e079be3cc642aa32553c8233fc1e0e7
